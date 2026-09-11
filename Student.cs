e161fdd7b0bce05d6cfc8d258adabfcbd385c5dd
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-11 15:47:04 +0200

    Jag är klar med den första delen av projktet. Har gjort klar Student.cs och skrivit alla förklaringar.

--- Student.cs
+++ Student.cs
@@ -15,39 +15,56 @@ class Student(string name)
     // Men denna lista kan vi fylla på allt efterhand.
     public List<Course> courses = [];
 
-        //Detta betyder att vi kan starta anropa metoden.
-        // Men eftersom vi har använt void så returnerar metoden ingeting.
-        //JoinCourse är själva metoden medan (Course coursesToJoin) är indata.
-        public void JoinCourse(Course coursesToJoin)
+    //Detta betyder att vi kan starta anropa metoden.
+    // Men eftersom vi har använt void så returnerar metoden ingeting.
+    //JoinCourse är själva metoden medan (Course coursesToJoin) är indata.
+    public void JoinCourse(Course coursesToJoin)
     {
         // if med utropstecken kontrolerar om kursen finns och att man inte kan 
         // gå med i samma kurs flera gånger.
         if (!courses.Contains(coursesToJoin))
         {
             //Här läggs kursen till stdentens lisa 
-            // och sedan meddelar man kursen att studenten har gått med
             courses.Add(coursesToJoin);
+            //// och sedan meddelar man kursen att studenten har gått med.
             coursesToJoin.Enroll(this);
         }
     }
-    
+    //Detta betyder att vi kan starta anropa metoden från andra klasser
+    // Men eftersom vi har använt void så returnerar metoden ingeting när vi anropar den.
+    // LeaveCourse är själva funktionen. (Course coursesToLeave) är indata.
     public void LeaveCourse(Course coursesToLeave)
     {
+        // Här kontrolleras if satsen om kursen fins innan den raderas.
         if (courses.Contains(coursesToLeave))
         {
+            //Här ta man bort kursen från studentens listan. 
             courses.Remove(coursesToLeave);
+            // Här meddelar man kursen att ta bort studenten.
             coursesToLeave.UnEnroll(this);
         }
     }
+    // Detta betyder att vi kan starta anropa metoden från andra klasser
+    // Men eftersom vi har använt void så returnerar metoden ingeting 
+    // när vi anropar den. Dem toma parateser saknar indata så 
+    // koden kommer bara att köras, när denna anropas.
     public void ScheduleCourse()
     {
+        // Här går koden igenom listan kurser som jag har valt att kalla c
         foreach (Course c in courses)
         {
+            // Här komme nu att skrivas ut varje kurs.
            Console.WriteLine(c.Name);
         }
     }
+    // Metoden ör nu nårbar för andra klasser eller fullt synlig.
+    // Eftersom ToSring har en fast metod eller standars så säger vi till 
+    // den att strunta i den och gör som jag vill istället.
+    // Eftersom vi inte har void så vill vi returnera ett värde.
     public override string ToString()
-    {
+    {    
+        // Här returneras den värde vi villa ha i detta fall
+        // Den studerandes namn.
         return Name;
     }
 
