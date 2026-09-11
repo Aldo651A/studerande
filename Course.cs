8aa2cb6a498e279b5981f261c639ace791e26aed
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-11 17:29:25 +0200

    Har börjat med Course.cs och lagt in primary constructor. Jag har även lagtt in if-satsen . Kommer att göra klar koden och därefter skriva föklaringarna på metoderna.

--- Course.cs
+++ Course.cs
@@ -1,8 +1,33 @@
-class Course(string name, string MaxSeats, string students)
+using System.ComponentModel;
+
+class Course(string name, int maxSeats)
 {
     public string Name = name;
+    public int MaxSeats = maxSeats;
+    public List<Student> students = [];
 
-    public List<Student> Students
+    public bool Enroll(Student studentToEnroll)
+    {
+        if (students.Count < MaxSeats)
+        {
+            students.Add(studentToEnroll);
+            Console.WriteLine("Studenten är nu registrerad ");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Kursen är tyvärr full. ")
+        }
+
+
+
+      
+      
+      
+      
+      
+        
+    }
 
 
 
