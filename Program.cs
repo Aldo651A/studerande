599b2cec44d20a2872da4577871661af3eae027c
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-17 15:21:42 +0200

    Har lagt till nya studenter för att testa max antalet.

--- Program.cs
+++ Program.cs
@@ -5,8 +5,19 @@ Student aldo = new Student("Aldo");
 Student ulf = new Student("Ulf");
 Student mia = new Student("Mia"); 
 
-Console.WriteLine("Testar om man kan gå med i kursen och dubblet skydd");
+Console.WriteLine("Testar om  man kan göra kopplingen från båda hållen samt dubblett ");
 programmering.Enroll(aldo); // Kursen lägg till mig 
 ulf.JoinCourse(programmering); // Studenten lägger till sig genom student
 programmering.Enroll(aldo); // Testar dubblett.
+
+Console.WriteLine("Testar max antalet studenter på kursen");
+Student anna = new Student("Anna");
+Student roger = new Student("Roger");
+Student lisa = new Student("Lisa");
+programmering.Enroll(anna);
+programmering.Enroll(roger);
+programmering.Enroll(lisa);
+programmering.Enroll(mia); // Det finns inga plater ledig 
+
+
 
